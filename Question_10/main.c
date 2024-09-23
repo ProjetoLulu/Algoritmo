7c8d470472fa446c4efd63f06750c7eff77cc18c
Giovana Gonçalves <G.jesusdasilvagoncalves@gmail.com>
2024-09-23 20:50:31 -0300

    renovação do codigo

--- Question_10/main.c
+++ Question_10/main.c
@@ -1,38 +1,51 @@
 #include <stdio.h>
 
-int main() {
-
-    int code;
-    float wage, new_wage, Rise;
-
-
-    printf("Enter the employee's current salary: ");
-    scanf("%f", &wage);
-
-    printf("Enter the employee's job code: ");
-    scanf("%d", &code);
+int main (){
+        int id;
+        float grade1, grade2, grade3, average;
+        char grade_letter;
 
 
-    if (code == 310) {
-            Rise = 0.05; // 5% de aumento
+  printf("Enter the student ID number: ");
+  scanf("%d", &id);
+  
+  printf("Enter the fisrt grade (30%% weight): ");
+  scanf("%f, &grade1");
+  
+  printf("Enter the second grade (30%% weight): ");
+  scanf("%f, &grade2");
+
+  printf("Enter the third grade (40%% weight): ");
+  scanf("%f, grade3");
+
+        average = (grade1 * 0.3) + (grade2 * 0.3) + (grade3 * 0.4);
+
+  if (average >= 90) {
+        grade_letter = 'A';
+
+  } else if (average >= 75) {
+        grade_letter = 'B';
+  
+  } else if (average >= 60) {
+        grade_letter = 'C';
+
+  } else if (average >= 40){
+        grade_letter = 'D';
+
+  } else {
+        grade_letter = 'E';
+  }
+
+   printf("\nStudent ID: %d\n", id);
+   printf("Average: %.2f\n", average);
+   printf("Grade: %c\n", grade_letter);
+
+  if (grade_letter == 'A' || grade_letter == 'B' || grade_letter == 'C') {
+        printf("Result: Passed\n");
+    } else {
+        printf("Result: Failed\n");
     }
 
-    else if (code == 456) {
-            Rise = 0.075; // 7,5% de aumento
-    }
-    else if (code == 885) {
-            Rise = 0.10; // 10% de aumento
-    }
-    else {
-            Rise = 0.15; // 15% de aumento para códigos não listados
-    }
-
-    new_wage = wage + (wage * Rise);
-
-    printf("wage old: %.2f\n", wage);
-    printf("New wage: %2f\n", new_wage);
-    printf("Difference: %2.f\n", new_wage - wage);
-
-        return 0;
-
+    return 0;
 }
+
